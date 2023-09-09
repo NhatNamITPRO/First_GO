7709bf24747ec713dade61907cf8d5e11b7fc6b3
NhatNamITPRO <nhatnamit214@gmail.com>
2023-09-10 01:14:36 +0700

    update

--- helloworld.go
+++ helloworld.go
@@ -1,6 +1,27 @@
 package main
-import ("fmt")
 
+import "fmt"
+
+type Move interface {
+	move() string
+}
+type Car struct {
+	name string
+}
+type Animal struct {
+	name string
+}
+
+func (c Car) move() string {
+	return "Moving by wheel"
+}
+func (a Animal) move() string {
+	return "Moving by leg"
+}
 func main() {
-  fmt.Println("Hello World!")
+	objs := []Move{Car{"BMW"},
+		Animal{"dog"}}
+	for _, obj := range objs {
+		fmt.Println(obj.move())
+	}
 }
